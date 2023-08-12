2541e9a6937acb282a0e8a7f1332dfac485b18f4
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-08-12 13:31:09 -0300

    signup form

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -1,23 +1,37 @@
 'use client';
 
-import React, { useState } from 'react';
 import {
   EyeInvisibleOutlined,
   EyeOutlined,
   MailOutlined,
 } from '@ant-design/icons';
-import { Button, Checkbox, Input } from '@nextui-org/react';
-import { Image } from '@nextui-org/react';
-import Link from 'next/link';
+import { Button, Checkbox, Image, Input, Link } from '@nextui-org/react';
+import React, { useState } from 'react';
 
-function LoginForm() {
+function SignUpForm() {
   const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   return (
     <form className='flex flex-col w-[400px] mx-auto gap-4 max-sm:w-[300px]'>
-      <h2 className='font-bold text-4xl mb-4'>Login</h2>
+      <h2 className='font-bold text-4xl mb-4'>SignUp</h2>
+      <div className='flex flex-col'>
+        <Input
+          type='text'
+          label='Name'
+          labelPlacement='outside'
+          placeholder='Jhon'
+        />
+      </div>
+      <div className='flex flex-col'>
+        <Input
+          type='text'
+          label='Surname'
+          labelPlacement='outside'
+          placeholder='Doe'
+        />
+      </div>
       <div className='flex flex-col'>
         <Input
           type='email'
@@ -48,44 +62,50 @@ function LoginForm() {
           }
         />
       </div>
-      <div className='flex justify-between mt-2'>
-        <div>
-          <Checkbox defaultSelected size='sm'>
-            Kepp me logged in
-          </Checkbox>
-        </div>
-        <div>
-          <Link
-            href='/PasswordRecovery'
-            className='text-sm p-1 text-primary font-semibold hover:text-red-400'
-          >
-            Forgot Password?
-          </Link>
-        </div>
+      <div className='flex flex-col'>
+        <Input
+          type={isVisible ? 'text' : 'password'}
+          label='Repeat Password'
+          labelPlacement='outside'
+          placeholder='y0urP4$sW0Rd'
+          endContent={
+            <button
+              className='focus:outline-none'
+              type='button'
+              onClick={toggleVisibility}
+            >
+              {isVisible ? (
+                <EyeInvisibleOutlined className='text-2xl text-default-400 pointer-events-none' />
+              ) : (
+                <EyeOutlined className='text-2xl text-default-400 pointer-events-none' />
+              )}
+            </button>
+          }
+        />
       </div>
       <Button fullWidth color='primary' className='text-white'>
-        Login
+        Register
       </Button>
       <p>
-        Don{`'`}t Have an Account?{' '}
+        Already Have an Account?{' '}
         <Link
-          href='/SignUp'
+          href='/Signin'
           className='text-primary font-semibold hover:text-red-400'
         >
-          Signup
+          Login
         </Link>{' '}
       </p>
     </form>
   );
 }
 
-function SignIn() {
+function SignUp() {
   return (
     <div className='container max-w-5xl mx-auto py-8'>
       <div className='flex p-4 max-md:flex-col'>
         <div className='flex flex-col max-md:hidden'>
           <Image
-            src='/formbg.jpg'
+            src='/form2bg.jpg'
             width='600px'
             height='auto'
             alt='pizza image'
@@ -93,11 +113,11 @@ function SignIn() {
           />
         </div>
         <div className='flex flex-col flex-1 justify-center align-middle px-12'>
-          <LoginForm />
+          <SignUpForm />
         </div>
       </div>
     </div>
   );
 }
 
-export default SignIn;
+export default SignUp;
